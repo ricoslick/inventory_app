dc6d2b98e0d544307832985bbb22efa088c85623
Ricoslick <bantuefwe@riseup.net>
2018-08-16 10:31:57 +0300

    ActionMailer smtp commit 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,8 @@ Rails.application.configure do
     authentication:    :plain,
     user_name:         ENV['SENDGRID_USERNAME'],
     password:          ENV['SENDGRID_PASSWORD'],
-    domain:            'heroku.com'
-    :enable_starttls_auto =>   true
+    domain:            'heroku.com',
+    enable_starttls_auto: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
