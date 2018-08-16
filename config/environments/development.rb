dcc355a3a54847a46d3720bd4482fd283afe6b86
Ricoslick <bantuefwe@riseup.net>
2018-08-16 15:06:06 +0300

    ActionMailer smtp commit 3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,11 +27,21 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'localhost:3000'                     # Local server
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'localhost:3000'                     # Local server
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    address:           'mail.riseup.net',
+    port:              '587',
+    authentication:    :plain,
+    user_name:         'bantuefwe',
+    password:          '$Wesakula*1588#',
+    domain:            'riseup.net',
+    enable_starttls_auto: true
+  }
+  
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
