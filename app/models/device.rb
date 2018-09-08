cbf3f54f5c185c28a41a4bb05aaadb34d221d270
Ricoslick <bantuefwe@riseup.net>
2018-09-08 16:01:43 +0300

    Device Users edit

--- app/models/device.rb
+++ app/models/device.rb
@@ -7,6 +7,7 @@ class Device < ApplicationRecord
   validates(:make, presence: true)
   validates(:model, presence: true)
   validates(:serial, presence: true, uniqueness: { case_sensitive: false })
+  validates(:customer_id, presence: true)
 
 	def self.search(query)
 		where("model || make || category ILIKE ?", "%#{query}%")
