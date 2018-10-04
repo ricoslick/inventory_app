c774360f6b2bee145175bd9864ac31b3d06b187b
Ricoslick <bantuefwe@riseup.net>
2018-10-04 15:51:08 +0300

    environmental variable security tweaks

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,13 @@ Bundler.require(*Rails.groups)
 module InventoryApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+
+    config.before_configuration do
+	  env_file = File.join(Rails.root, 'config', 'local_env.yml')
+	  YAML.load(File.open(env_file)).each do |key, value|
+	    ENV[key.to_s] = value
+	  end if File.exists?(env_file)
+	end
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
@@ -17,3 +24,4 @@ module InventoryApp
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+# Environmental variable db config
