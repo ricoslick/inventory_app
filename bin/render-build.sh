de8bbd11d575f2ab4aa6318554ffe924e038428e
Eric Wasike <ewj@EAPI.local>
2023-02-26 15:56:45 +0300

    render-build changes v7

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,6 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-rails db:environment:set RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
-bundle exec rake db:migrate:reset
+rails db:environment:set RAILS_ENV=production 
+bundle exec rake db:migrate:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rake db:migrate
