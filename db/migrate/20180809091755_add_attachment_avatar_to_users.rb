8dc416ae9fae0484fe2e53e769ce194bdd578358
Eric Wasike <ewj@EAPI.local>
2023-03-07 21:13:00 +0300

    attachment edit v1

--- db/migrate/20180809091755_add_attachment_avatar_to_users.rb
+++ db/migrate/20180809091755_add_attachment_avatar_to_users.rb
@@ -1,9 +1,9 @@
 class AddAttachmentAvatarToUsers < ActiveRecord::Migration[5.1]
-  def up
-    add_column :users, :avatar
+  def self.up
+    add_attachment :users, :avatar
   end
 
-  def down
-    remove_column :users, :avatar
+  def self.down
+    remove_attachment :users, :avatar
   end
 end
